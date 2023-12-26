227cad4e537161f13970b098c832f932f8d70025
Bisma Bendesa <bismabendesajaya@gmail.com>
2023-12-26 11:47:17 +0800

    Create the general UI

--- main.py
+++ main.py
@@ -7,3 +7,12 @@ Please input your sentences in bahasa:
 """)
 
 input_sentence = st.text_input('Input Sentence')
+input_sentence_btn = st.button("Submit", type="primary")
+
+st.write(" ")
+st.subheader("Result:")
+st.write("valid / not valid")
+st.subheader("Sentence Structure")
+st.write("blalblalbal")
+st.subheader("Parse Tree:")
+st.write("blalblalbal")
