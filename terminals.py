b3dd89ec026804c319609f695281b396dece91d2
Bisma Bendesa <bismabendesajaya@gmail.com>
2023-12-26 12:21:09 +0800

    add terminal symbols

---
+++ terminals.py
@@ -0,0 +1,12 @@
+# Terminal symbols
+
+terminals = {
+  'Prep' : ['di', 'ke', 'dari', 'pada', 'dengan', 'tanpa', 'untuk', 'sampai', 'sejak', 'hingga'],
+  'Num' : ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'],
+  'Adj' : ['manis', 'pahit', 'besar', 'baru', 'biru', 'merah', 'panas', 'dingin'],
+  'Adv' : ['sangat', 'agak', 'jarang', 'sudah', 'akan', 'sedang'],
+  'Noun': ['Putra', 'Saya', 'Burung', 'Tebu', 'Rasa', 'Pohon', 'Hutan', 'tanaman', 'kebun', 'bunga'],
+  'Verb': ['bermain', 'memasak', 'memberi', 'belajar', 'membantu', 'menjaga', 'berkelahi', 'mengambil', 'meniru'],
+  'Pronoun': ['ini', 'itu', 'saya', 'dia', 'mereka', 'kami', 'kita', 'saudara'],
+  'PropNoun': ['Agung', 'Kali Unda', "denpasar", "New york"],
+}
